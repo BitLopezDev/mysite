fa71c4c5078a5579d1bf2dac0e750be6820a9b52
Santiago C <3glbbv5pg@mozmail.com>
2023-04-30 17:24:06 -0300

    Revert "Attempt to fix bug 1"
    This reverts commit 17209430f6ac346cf625909d5849fa52f333f931.

--- src/components/Sections/NextuCourses.tsx
+++ src/components/Sections/NextuCourses.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react'
+import React, { Component } from 'react'
 import { Menu, Segment } from 'semantic-ui-react'
 import './../../styles/fullstack.module.scss';
 // ./../../assets/users/Santiago/credenciales/NextU/PHP_OOP.png
@@ -18,20 +18,19 @@ import './../../scripts/index';*/
 function hideElement(name, ...others) {
 
     //document.getElementById(elementsofaccordion[Object.keys(elementsofaccordion)[i]].li).classList.remove('active');
-    
+
     //document.getElementById('carousel-item-1').classList.contains("active")
-    //if (document.getElementById(name)?.classList?.contains("seesconde")) {
-    
+    if (document.getElementById(name)?.classList?.contains("seesconde")) {
         document.getElementById(name)?.classList?.remove('seesconde');
-    //}
+    }
     //console.log(others);
 
 
     for (let i = 0; i < others.length; i++) {
         // const StringArray = othersArray[i];
-       // if (!document.getElementById(others[i])?.classList.contains("seesconde")) {
+        if (!document.getElementById(others[i])?.classList.contains("seesconde")) {
             document.getElementById(others[i])?.classList.add('seesconde');
-        //}
+        }
 
     }
     //console.log(othersArray);
@@ -51,29 +50,13 @@ class NextuCourses extends Component {
         console.log(name);
         switch (thename) {
             case 'Desarrollador Web 2016':
-           
-                   // hideElement('wd', 'FrontEnd', 'otros');
-       
-                    document.getElementById('wd')?.classList?.remove('seesconde');
-                    document.getElementById('otros')?.classList.add('seesconde');
-                    document.getElementById('FrontEnd')?.classList.add('seesconde');
+                hideElement('wd', 'FrontEnd', 'otros');
                 break;
             case 'Frontend con JS 2022':
-               
-          document.getElementById('FrontEnd')?.classList?.remove('seesconde');
-          document.getElementById('otros')?.classList.add('seesconde');
-          document.getElementById('wd')?.classList.add('seesconde');
-          
-                //hideElement('FrontEnd', 'otros', 'wd');
-      
+                hideElement('FrontEnd', 'wd', 'otros');
                 break;
             case 'Otros Cursos 2022':
-                document.getElementById('otros')?.classList?.remove('seesconde');
-                document.getElementById('FrontEnd')?.classList.add('seesconde');
-                document.getElementById('wd')?.classList.add('seesconde');
-                    //hideElement('otros', 'FrontEnd', 'wd');
-              
-                
+                hideElement('otros', 'FrontEnd', 'wd');
                 break;
         }
 
