de71277dc16b19da33a0d68231269dd637e7a690
Santiago C <santiagocomesana03@gmail.com>
2023-06-22 20:36:52 -0300

    step 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,10 +25,56 @@ const items = [
   },
 ];
 //FIXME: Fix multile calls to Layout
+
 function HomePage() {
   return (
     <>
-    
+    <Head>
+    <title>Santiago Comesaña | index</title>
+  </Head>
+  <Layout nav={true} paddTop={75} > </Layout>
+<center>
+      <h1 className="antigradienttext">
+        <i>Santiago Comesaña López</i>
+      </h1>
+      <h4>BitLopez</h4>
+
+    </center>
+    <div className="container mt-5">
+    <div className="row">
+    <div className="col-sm-4">
+        <h2>Sobre mí</h2>
+
+        <Image src={Object.entries(Logo)[0].slice(1)} width={150} />
+        <p>Desarrollador web Full Stack con JavaScript, TypeScript, PHP y experiencia en cyberseguridad con formación
+          que lo respalda.</p>
+        {/* <p></p>
+
+        <h3 className="mt-4"> </h3>
+        <p></p> */}
+        <div className='hideonmobile'>
+          <ul className="nav nav-pills flex-column ">
+            <li className="nav-item">
+              <a className="nav-link active" href="/">Inicio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Desarrollador web FullStack</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="MSP">BitLopez MSP</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="/Labs">Labs</a>
+            </li>
+          </ul>
+
+        </div>
+        <hr className="d-sm-none" />
+      </div>
+
+    </div>
+
+    </div>
     </>
   );
 }
