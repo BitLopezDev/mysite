e104251d18bfa5aaeea62b30cdcf6d3ab917b6e6
Santiago C <santiagocomesana03@gmail.com>
2023-06-28 15:54:28 -0300

    Abstracted and Cleaned MSP

--- src/pages/Security.tsx
+++ src/pages/Security.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import Layout from '../components/Layout/Layout';
 import { SecurityCards } from '../components/Sections/SecurityCards';
 import { PlatziSecurity } from '../components/Sections/PlatziSecurity';
-import { UdemySecurity } from '../components/Sections/UdemySecurity';
 import Head from 'next/head';
+import { UdemySecurity } from '../components/Sections/UdemySecurity';
+
 import './../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 
