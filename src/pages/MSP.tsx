b7ce56aafbb8c5d8fe773a3b56ad8cdfa99b0cb5
Santiago C <santiagocomesana03@gmail.com>
2023-07-16 17:33:39 -0300

    Error 1 more precise 1 firx 1

--- src/pages/MSP.tsx
+++ src/pages/MSP.tsx
@@ -7,7 +7,7 @@ import { MSPBootAnimation } from "../components/Sections/innerSections/msp/mspbo
 import { MSPVendors } from "../components/Sections/innerSections/msp/mspvendors";
 import { MSPExtra1 } from "../components/Sections/innerSections/msp/mspextra1";
 import Sophos from "./../../public/assets/users/Santiago/services/Sophos.ico";
-import IBM from "./../../public/images/IBM.jpg";
+import IBM from "./../../public/images/IBM.jpeg";
 import Intezer from "./../../public/images/Intezer.png";
 import Fortinet from "./../../public/images/Fortinet.jpg";
 import Kaspersy from "./../../public/images/kaspersky.png";
