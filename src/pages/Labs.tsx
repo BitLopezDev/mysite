12203e05675cdf4fd08e0e6f339ae52d761ef630
Santiago C <santiagocomesana03@gmail.com>
2023-07-24 18:35:39 -0300

    Labs 3.0

--- src/pages/Labs.tsx
+++ src/pages/Labs.tsx
@@ -95,7 +95,7 @@ export default function Labs() {
 <br />
 <center>
 <div className="container">
-  <h2 className='colorwhite'>Parte de nuestro Arcenal</h2>
+  <h1 className='colorwhite'>Parte de nuestro Arcenal</h1>
 <div className="row">
     <div className="col">
     <div className="card">
@@ -116,81 +116,147 @@ export default function Labs() {
   </div>
   <br />
   <div className="row">
+  <h3 className='colorwhite'>Centralización, respuesta y automatización</h3>
+
+<br />
     <div className="col-md-4">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">SOAR</h5>
-          <p className="card-text">Orquestación y automatización de respuestas de su SOC</p>
+          <h5 className="card-title" data-bs-toggle="collapse" data-bs-target="#soar">SOAR</h5>
+          <p className="card-text">Orquestación y automatización de respuestas de su SOC.</p>
+          <br />
+          <h6><a data-bs-toggle="collapse" data-bs-target="#soar"> Expandir / Colapsar</a></h6>
+          <div id="soar" className="collapse">
+          Proteja su negocio con seguridad orquestada y automática. Por la abrumante cantidad de alertas, los equipos de seguridad revisan en promedio la mitad de las alertas de seguridad. Evite agotar a su equipo y obtener los mejores resultados con SOAR. A medida que las empresas crecen, se van dando cuenta de la necesidad de SIEM y SOAR.
+<br /><b>Hoy, la seguridad está en sus manos.</b>
+  </div>
         </div>
       </div>
     </div>
     <div className="col-md-4">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">XDR </h5>
+          <h5 className="card-title" data-bs-toggle="collapse" data-bs-target="#xdr">XDR</h5>
           <p className="card-text">Rápida respuesta a eventos y recopilación de datos.</p>
+          <br />
+          <h6><a data-bs-toggle="collapse" data-bs-target="#xdr"> Expandir / Colapsar</a></h6>
+          <div id="xdr" className="collapse">
+          Proteja sus Endpoints con seguridad premiada por autónomos de los líderes de la industra Bitdefender y Sophos, antivirus Next-Gen con Endpoint Detection and Repsonse (EDR) y eXtended Detection and Response (XDR, también conocido como NEXT-GEN SIEM) en el núcleo para asegurar la mejor defensa para sus endpoints y su red.
+<br /><b>Hoy, la seguridad está en sus manos.</b>
+  </div>
         </div>
       </div>
     </div>
     <div className="col-md-4">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">SIEM</h5>
-          <p className="card-text">Centralice notificaciones y evite la fatiga por exceso de alertas</p>
+          <h5 className="card-title" data-bs-toggle="collapse" data-bs-target="#siem">SIEM</h5>
+          <p className="card-text">Centralice notificaciones y evite la fatiga por exceso de alertas.</p>
+          <br />
+          <h6><a data-bs-toggle="collapse" data-bs-target="#siem"> Expandir / Colapsar</a></h6>
+          <div id="siem" className="collapse">
+          Proteja su organización reconociendo amenazas externas o internas, vulnerabilidades y operaciones maliciosas antes de que interrumpas sus operaciones comerciales, violen la continuidad de negocio y lo perjudiquen económica, reputacional o, incluso, legalmente.
+Para ello utilizmos Microsoft Azure Sentinel: un SIEM que utiliza Inteligencia Artificial de punta para protegerlo a usted y a su nnegocio.
+<br /><b>Hoy, la seguridad está en sus manos.</b>
+  </div>
         </div>
       </div>
     </div>
     </div>
     <br /> <br />
     <div className="row">
+    <h3 className='colorwhite'>Protección de red y recursos</h3>
+
+<br />
     <div className="col-md-4">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">IPS</h5>
-          <p className="card-text">Prevensión de intrusos Next Gen para proteger su red y sus recursos</p>
+          <h5 className="card-title" data-bs-toggle="collapse" data-bs-target="#ips"> NEXT-GEN IPS</h5>
+          <p className="card-text">Prevensión de intrusos Next Gen para proteger su red y sus recursos.</p>
+          <br />
+          <h6><a data-bs-toggle="collapse" data-bs-target="#ips"> Expandir / Colapsar</a></h6>
+          <div id="ips" className="collapse">
+    Bloquee tráfico sospechos, amenazas, y proteja su red y sus recursos más valiosos con un IPS de próxima Generación.
+    <br /><b>Hoy, la seguridad está en sus manos.</b>
+  </div>
         </div>
       </div>
     </div>
     <div className="col-md-4">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">Next-Gen Firewall</h5>
-          <p className="card-text">Firewalls <i>Powered by</i> Inteligencia Artificial con tecnología de punta</p>
+          <h5 className="card-title" data-bs-toggle="collapse" data-bs-target="#fw">NEXT-GEN FIREWALL</h5>
+          <p className="card-text">Firewalls Powered by Inteligencia Artificial con tecnología de punta.</p>
+          <br />
+          <h6><a data-bs-toggle="collapse" data-bs-target="#fw"> Expandir / Colapsar</a></h6>
+          <div id="fw" className="collapse">
+          Defienda sus endpoints, servidores, dispositivos IoT, recursos, aplicaciones, correo y red con un Firewall de red Next-Gen, con Sistema de Prevensión de Intrusos Next-Gen (IPS), análisis dinámico de archivos y DLP incorporado contra ataques de día cero, exploits, ransomware, phising, fraude, virus, rootkits, troyanos, movimiento lateral, acceso de terceros no autorizados, entre tantas otras cosas.
+<br /><b>Hoy, la seguridad está en sus manos.</b>
+  </div>
         </div>
       </div>
     </div>
     <div className="col-md-4">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">NDR</h5>
-          <p className="card-text">Defienda su red de movimiento lateral, DDoS y responda rápidamente</p>
+          <h5 className="card-title" data-bs-toggle="collapse" data-bs-target="#ndr">NDR</h5>
+          <p className="card-text">Defienda su red de movimiento lateral, DDoS y responda rápidamente.</p>
+          <br />
+          <h6><a data-bs-toggle="collapse" data-bs-target="#ndr"> Expandir / Colapsar</a></h6>
+          <div id="ndr" className="collapse">
+    Mejore la visibilidad del tráfico de su red, obtenga más detalles sobre las amenazas y responda con mayor eficacia, eficiencia y velocidad para mantener seguros sus recursos y su red.
+    <br /><b>Hoy, la seguridad está en sus manos.</b>
+  </div>
         </div>
       </div>
     </div>
   </div>
   <br /> <br />
   <div className="row">
-    <div className="col-md-4">
+  <h3 className='colorwhite'>Arquitecturas, modelos, recursos</h3>
+
+<br />
+  <div className="col-md-4">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">SASE</h5>
-          <p className="card-text">Centralice su seguridad en una arquitectura idónea para ZERO TRUST</p>
+          <h5 className="card-title" data-bs-toggle="collapse" data-bs-target="#sase">SASE</h5>
+          <p className="card-text">Centralice su seguridad en una arquitectura idónea para ZERO TRUST.</p>
+          <br />
+          <h6><a data-bs-toggle="collapse" data-bs-target="#sase"> Expandir / Colapsar</a></h6>
+          <div id="sase" className="collapse">
+          Defienda su red y sus recursos sin perder capacidad de conectar a sus usuarios, sistemas, endpoints, servidores y redes externas a aplicaciones y recursos aumentando la capacidad del tradicional SD-WAN siéndole fiel a la filosofía Zero Trust.
+<br /><b>Hoy, la seguridad está en sus manos.</b>
+  </div>
         </div>
       </div>
     </div>
     <div className="col-md-4">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">Zero Trust</h5>
-          <p className="card-text">Use el modelo de seguridad más efectivo y proteja su red y sus recursos</p>
+          <h5 className="card-title" data-bs-toggle="collapse" data-bs-target="#zt">ZERO TRUST</h5>
+          <p className="card-text">Use el modelo de seguridad más efectivo y proteja su red y sus recursos.</p>
+          <br />
+          <h6><a data-bs-toggle="collapse" data-bs-target="#zt"> Expandir / Colapsar</a></h6>
+          <div id="zt" className="collapse">
+          Defienda su red, sus recursos y su organización a través de una arquitectura ZERO TRUST. Impida el acceso de dispositivos y personal cuyas credenciales sean inválidas o no se hayan verificado.
+Gracias a software como el ZTNA de Sophos Labs, se puede garantizar que quien accede a la red y a los recursos es el usuario autorizado en el dispositivo autorizado y que este está libre de cualquier programa malicioso que pudiere representar una amenaza para su organización, su red, sus recursos y la continuidad del negocio.
+<br /><b>Hoy, la seguridad está en sus manos.</b>
+</div>
         </div>
       </div>
     </div>
     <div className="col-md-4">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">Inline Sandbox</h5>
-          <p className="card-text">Defienda su organización de amenazas <i>0-day</i> sin afectar sus recursos</p>
+          <h5 className="card-title" data-bs-toggle="collapse" data-bs-target="#sand">Threat Intelligence</h5>
+          <p className="card-text">Defienda su organización de amenazas 0-day sin afectar sus recursos.</p>
+          <br />
+          <h6><a data-bs-toggle="collapse" data-bs-target="#sand"> Expandir / Colapsar</a></h6>
+          <div id="sand" className="collapse">
+          Manténgase un paso más adelante que las ciberamenazas actuales, incluso las más complejas, con Cyber Threat Intelligence.
+Herramientas como Microsoft Defender Threat Intelligence, Intezer, Kaspersky Threat Intelligence Portal y VirusTotal a su alcance para mantenerlo seguro a usted, sus empleados, y su negocio.
+<br /><b>Hoy, la seguridad está en sus manos.</b>
+</div>
         </div>
       </div>
     </div>
