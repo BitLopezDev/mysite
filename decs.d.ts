3409e6c74c12e3e4f580455f1ad9a7ac798cedd1
BitLopezDev <contacto@bitopez.bio>
2024-01-24 11:13:55 -0300

    attempt 4

--- decs.d.ts
+++ decs.d.ts
@@ -1,1 +1,6 @@
-declare module "semantic-ui-react"
+declare module "semantic-ui-react";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.ico";
+export {};
